81fcdf572660a96d25ac7bd414d64446425f3187
Shahrad Elahi <shahradq@gmail.com>
2023-12-27 19:41:23 +0330

    project init

---
+++ tsup.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from 'tsup';
+
+export default defineConfig({
+  clean: true,
+  dts: true,
+  entry: ['src/index.ts'],
+  format: ['cjs', 'esm'],
+  sourcemap: true,
+  target: 'esnext',
+  outDir: 'dist'
+});
