9f4bc5ec4267b7ef1c1ed408807354cfa85e2752
Shahrad Elahi <shahrad@litehex.com>
2025-03-01 22:17:09 +0000

    refactor: Using `@se-oss/msgpack` package in `MSGPack` parser

--- src/parser/msg-pack.ts
+++ src/parser/msg-pack.ts
@@ -1,29 +1,15 @@
-import { decode, encode } from '@msgpack/msgpack';
+import { default as _MSGPack } from '@se-oss/msgpack';
 
 import type { Serializable } from '@/typings';
 import { toMap, toPlainObject } from '@/utils/object';
 
 export class MSGPack {
   public static parse(data: any): Map<string, Serializable> {
-    if (typeof data !== 'string') {
-      throw new TypeError('MessagePack data must be a string encoded in base64');
-    }
-    const buffer = Buffer.from(data, 'base64');
-    const decoded = decode(buffer);
-
-    if (!decoded || typeof decoded !== 'object') {
-      throw new TypeError('Malformed MessagePack data');
-    }
-
+    const decoded = _MSGPack.parse(data) as object;
     return toMap(decoded);
   }
 
   public static stringify(data: any): string {
-    if (typeof data !== 'object') {
-      throw new TypeError('data must be an object');
-    }
-    const encoded = encode(toPlainObject(data));
-    const buffer = Buffer.from(encoded.buffer, encoded.byteOffset, encoded.byteLength);
-    return buffer.toString('base64');
+    return _MSGPack.stringify(toPlainObject(data));
   }
 }
